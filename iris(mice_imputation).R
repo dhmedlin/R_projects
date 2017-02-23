aebe3ff14ccbb1a3ec3c01d7b4c1783c04ce1987
Devon Medlin <devonhmedlin@gmail.com>
2017-02-23 14:55:22 +0100

    added pooling

--- iris(mice_imputation).R
+++ iris(mice_imputation).R
@@ -1,4 +1,4 @@
-library_loading <- c("Hmisc", "missForest", "caTools", "mice", "dplyr")
+library_loading <- c("Hmisc", "missForest", "caTools", "mice", "dplyr", "ggplot2", "cowplot")
 lapply(library_loading, require, character.only = T) #load packages
 
 data("iris") 
@@ -23,10 +23,10 @@ summary(test) #check for NA's
 
 full <- rbind(train, test) #row bind train and test
 
-#---------------------------------------------------
+#---------------------------------------------------------------
 #imputation
 
-ini <- mice(full, maxit=0, pri=F)
+ini <- mice(full, maxit= 0, pri= F, seed = 33)
 pred <- ini$pred
 pred["Species",] <- 0
 meth1 <- c("pmm", "pmm", "pmm", "pmm", "")
@@ -36,9 +36,9 @@ imp$imp$Species
 
 summary(imp)
 
-imp$imp$Sepal.Length #shows the 5 imputed values for each missing variable in the Sepal.Length column
+imp$imp$Sepal.Length #shows the 5 imputed values for each missing value in the Sepal.Length column
 
-#---------------------------------------------------
+#---------------------------------------------------------------
 #imputed vs observed visualizations
 
 xyplot(imp, Sepal.Length ~ Sepal.Width+Petal.Length+Petal.Width) #scatterplot of observed vs actual
@@ -48,5 +48,27 @@ densityplot(imp) #density plot of each of the 5 imputed datasets (red) vs the ob
 
 stripplot(imp, pch = 20) #plot by each data point
 
-#---------------------------------------------------
+#---------------------------------------------------------------
 #pooling
+
+mice_output <- complete(imp) #adds mice data to the full df 
+
+p1 <- ggplot(full, aes(Sepal.Length) ) + 
+  geom_histogram(binwidth = 0.5, color = "white", fill = "light blue") +
+  ggtitle("Sepal.Length: Original") 
+#original data  
+  
+p2<- ggplot(mice_output, aes(Sepal.Length) ) + 
+  geom_histogram(binwidth = 0.5, color = "white", fill = "dark blue") +
+  ggtitle("Sepal.Length: MICE")
+#mice data
+
+plot_grid(p1, p2, align = "h") #original vs mice histogram of sepal.length
+
+full$Sepal.Length <- mice_output$Sepal.Length
+full$Sepal.Width <- mice_output$Sepal.Width
+full$Petal.Length <- mice_output$Petal.Length
+full$Petal.Width <- mice_output$Petal.Width
+#replace original variables with the variables from the mice model
+
+sum(is.na(full)) #check for NA's
